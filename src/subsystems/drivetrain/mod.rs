b2ce98a8aaa87af0cd84be9bdaa8e5f6fa7f3d98
zabackary <137591653+zabackary@users.noreply.github.com>
2025-04-28 16:29:44 +0900

    feat: add DrivetrainActionFuture

--- src/subsystems/drivetrain/mod.rs
+++ src/subsystems/drivetrain/mod.rs
@@ -1,8 +1,9 @@
-use core::{cell::RefCell, mem};
+use core::{cell::RefCell, future::Future, sync::atomic::AtomicBool};
 
 use alloc::{boxed::Box, rc::Rc};
-use vexide::sync::Barrier;
 use vexide_motorgroup::MotorGroup;
+
+use crate::utils::pose::Pose;
 
 use super::tracking::TrackingSubsystem;
 
@@ -11,10 +12,44 @@ mod voltage_pair;
 
 pub use voltage_pair::VoltagePair;
 
+#[allow(clippy::type_complexity)]
+pub struct DrivetrainActionFuture {
+    settled: Rc<AtomicBool>,
+    tracking: Rc<RefCell<TrackingSubsystem>>,
+    callback: Option<RefCell<Box<dyn FnMut(Pose)>>>,
+}
+
+impl Future for DrivetrainActionFuture {
+    type Output = ();
+
+    fn poll(
+        self: core::pin::Pin<&mut Self>,
+        cx: &mut core::task::Context<'_>,
+    ) -> core::task::Poll<Self::Output> {
+        if self.settled.load(core::sync::atomic::Ordering::Acquire) {
+            core::task::Poll::Ready(())
+        } else {
+            cx.waker().wake_by_ref();
+            if let Some(callback) = &self.callback {
+                (callback.borrow_mut())(self.tracking.borrow().pose());
+            }
+            core::task::Poll::Pending
+        }
+    }
+}
+
+impl DrivetrainActionFuture {
+    pub fn with_callback(mut self, callback: impl FnMut(Pose) + 'static) -> Self {
+        self.callback = Some(RefCell::new(Box::new(callback)));
+        self
+    }
+}
+
+#[allow(clippy::type_complexity)]
 pub struct Drivetrain {
-    action: Rc<RefCell<Option<Box<dyn actions::Action>>>>,
-    action_finish_barrier: Rc<RefCell<Option<vexide::sync::Barrier>>>,
+    action: Rc<RefCell<Option<(Box<dyn actions::Action>, Rc<AtomicBool>)>>>,
     max_voltage: Rc<RefCell<f64>>,
+    tracking: Rc<RefCell<TrackingSubsystem>>,
     _task: vexide::task::Task<()>,
 }
 
@@ -28,69 +63,64 @@ impl Drivetrain {
         tracking: Rc<RefCell<TrackingSubsystem>>,
     ) -> Self {
         let action = Rc::new(RefCell::new(None));
-        let action_finish_barrier = Rc::new(RefCell::new(None));
         let max_voltage = Rc::new(RefCell::new(max_voltage));
         Drivetrain {
             action: action.clone(),
-            action_finish_barrier: action_finish_barrier.clone(),
             max_voltage: max_voltage.clone(),
+            tracking: tracking.clone(),
             _task: vexide::task::spawn(async move {
                 loop {
-                    let mut action_owned = action.borrow_mut();
-                    if let Some(ref mut action_ref) = *action_owned {
-                        // Get the tracking position
-                        let tracking = tracking.borrow();
-                        let position = tracking.pose();
-                        let mut left = left.borrow_mut();
-                        let mut right = right.borrow_mut();
-                        // Assemble the action context
-                        let context = actions::ActionContext {
-                            left_offset: left
-                                .position()
-                                .map(|x| x.as_revolutions() * wheel_circumference)
-                                .unwrap_or(0.0),
-                            right_offset: right
-                                .position()
-                                .map(|x| x.as_revolutions() * wheel_circumference)
-                                .unwrap_or(0.0),
-                            left_velocity: left.velocity().unwrap_or(0.0) * wheel_circumference,
-                            right_velocity: right.velocity().unwrap_or(0.0) * wheel_circumference,
-                            pose: position,
-                        };
-                        // Run the action
-                        if let Some(mut voltage) = action_ref.update(context) {
-                            // If the action is still running
-                            if tracking.reverse() {
-                                // Rotate the robot in the opposite direction
-                                // if the tracking subsystem is reversed
-                                voltage = voltage.reverse();
-                            }
-                            // Scale the voltage to be under the max voltage
-                            voltage = voltage.max_voltage(*max_voltage.borrow());
-                            // Set the voltage
-                            if let Err(e) = left.set_voltage(voltage.left) {
-                                log::error!("Failed to set left voltage: {:?}", e);
-                            }
-                            if let Err(e) = right.set_voltage(voltage.right) {
-                                log::error!("Failed to set right voltage: {:?}", e);
-                            }
-                            drop(action_owned);
-                        } else {
-                            // Zero out the motors if the action is done
-                            _ = left.set_voltage(0.0);
-                            _ = right.set_voltage(0.0);
-                            *action_owned = None;
-                            mem::drop(action_owned);
-                            mem::drop(left);
-                            mem::drop(right);
-                            mem::drop(tracking);
-                            // Notify the main task that the action is done
-                            if let Some(barrier) = action_finish_barrier.borrow().as_ref() {
-                                barrier.wait().await;
+                    {
+                        let mut action_owned = action.borrow_mut();
+                        if let Some(ref mut action_ref) = *action_owned {
+                            // Get the tracking position
+                            let tracking = tracking.borrow();
+                            let position = tracking.pose();
+                            let mut left = left.borrow_mut();
+                            let mut right = right.borrow_mut();
+                            // Assemble the action context
+                            let context = actions::ActionContext {
+                                left_offset: left
+                                    .position()
+                                    .map(|x| x.as_revolutions() * wheel_circumference)
+                                    .unwrap_or(0.0),
+                                right_offset: right
+                                    .position()
+                                    .map(|x| x.as_revolutions() * wheel_circumference)
+                                    .unwrap_or(0.0),
+                                left_velocity: left.velocity().unwrap_or(0.0) * wheel_circumference,
+                                right_velocity: right.velocity().unwrap_or(0.0)
+                                    * wheel_circumference,
+                                pose: position,
+                            };
+                            // Run the action
+                            if let Some(mut voltage) = action_ref.0.update(context) {
+                                // If the action is still running
+                                if tracking.reverse() {
+                                    // Rotate the robot in the opposite direction
+                                    // if the tracking subsystem is reversed
+                                    voltage = voltage.reverse();
+                                }
+                                // Scale the voltage to be under the max voltage
+                                voltage = voltage.max_voltage(*max_voltage.borrow());
+                                // Set the voltage
+                                if let Err(e) = left.set_voltage(voltage.left) {
+                                    log::error!("Failed to set left voltage: {:?}", e);
+                                }
+                                if let Err(e) = right.set_voltage(voltage.right) {
+                                    log::error!("Failed to set right voltage: {:?}", e);
+                                }
+                                drop(action_owned);
+                            } else {
+                                // Zero out the motors if the action is done
+                                _ = left.set_voltage(0.0);
+                                _ = right.set_voltage(0.0);
+                                // Notify the main task that the action is done
+                                action_ref
+                                    .1
+                                    .store(true, core::sync::atomic::Ordering::SeqCst);
                             }
                         }
-                    } else {
-                        mem::drop(action_owned);
                     }
                     vexide::time::sleep(core::time::Duration::from_millis(10)).await;
                 }
@@ -100,7 +130,10 @@ impl Drivetrain {
 
     pub fn set_voltage(&mut self, voltage: VoltagePair) {
         let mut action = self.action.borrow_mut();
-        *action = Some(Box::new(actions::VoltageAction { voltage }));
+        *action = Some((
+            Box::new(actions::VoltageAction { voltage }),
+            Rc::new(AtomicBool::new(false)),
+        ));
     }
 
     pub fn set_max_voltage(&mut self, max_voltage: f64) {
@@ -108,24 +141,21 @@ impl Drivetrain {
         *max_voltage_ref = max_voltage;
     }
 
-    pub async fn boxed_action(&mut self, new_action: Box<dyn actions::Action>) {
+    pub fn boxed_action(&mut self, new_action: Box<dyn actions::Action>) -> DrivetrainActionFuture {
         let mut action = self.action.borrow_mut();
-        *action = Some(new_action);
+        let bool = Rc::new(AtomicBool::new(false));
+        *action = Some((new_action, bool.clone()));
         drop(action);
 
-        let barrier = Barrier::new(2);
-        *self.action_finish_barrier.borrow_mut() = Some(barrier);
-
-        self.action_finish_barrier
-            .borrow()
-            .as_ref()
-            .unwrap()
-            .wait()
-            .await;
+        DrivetrainActionFuture {
+            callback: None,
+            settled: bool,
+            tracking: self.tracking.clone(),
+        }
     }
 
-    pub async fn action(&mut self, action: impl actions::Action + 'static) {
-        self.boxed_action(Box::new(action)).await;
+    pub fn action(&mut self, action: impl actions::Action + 'static) -> DrivetrainActionFuture {
+        self.boxed_action(Box::new(action))
     }
 
     pub fn cancel_action(&mut self) {
