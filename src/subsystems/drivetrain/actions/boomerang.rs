87956330d30ad21f7bf42d061aa16acaec9d3ff2
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-02 22:35:50 +0900

    feat: add reversed to boomerang

--- src/subsystems/drivetrain/actions/boomerang.rs
+++ src/subsystems/drivetrain/actions/boomerang.rs
@@ -20,6 +20,9 @@ pub struct BoomerangAction {
     /// This prevents oscillation when we are very close to the target point.
     close: f64,
 
+    /// Whether this action is reversed
+    reverse: bool,
+
     tolerances: Tolerances,
 
     linear_pid: Pid<f64>,
@@ -40,7 +43,17 @@ impl BoomerangAction {
             tolerances: config.linear_tolerances(),
             linear_pid: config.linear_pid(0.0),
             angular_pid: config.turn_pid(0.0),
+            reverse: false,
         }
+    }
+
+    /// Sets this action to be reversed.
+    ///
+    /// When reversed, the robot will drive backwards to the target point,
+    /// attempting to face the *back* of the robot towards the target heading.
+    pub fn reversed(mut self) -> Self {
+        self.reverse = true;
+        self
     }
 }
 
@@ -62,7 +75,14 @@ impl super::Action for BoomerangAction {
         let angle_to_target = Angle::from_radians(local_target.y.atan2(local_target.x));
 
         // Compute the angular angle
-        let error_anglar = (angle_to_target - context.data.heading).wrapped_half();
+        let error_angular = (angle_to_target
+            - if self.reverse {
+                // If we're reversed, we want to face backwards
+                context.data.heading + Angle::HALF_TURN
+            } else {
+                context.data.heading
+            })
+        .wrapped_half();
         let (error_distance, close) = {
             let norm = local_target.norm();
             let close = norm < self.close;
@@ -82,13 +102,15 @@ impl super::Action for BoomerangAction {
             0.0
         } else {
             self.angular_pid
-                .next_control_output(error_anglar.as_radians())
+                .next_control_output(error_angular.as_radians())
                 .output
         };
         let output_linear = self.linear_pid.next_control_output(-error_distance).output
             // If the angular error is more than 90 degrees (|cos(angle) < 0|),
             // stop moving forward
-            * error_anglar.cos().max(0.0);
+            * error_angular.cos().max(0.0)
+            // If reversed, invert the linear output to drive backwards
+            * if self.reverse { -1.0 } else { 1.0 };
 
         Some(DrivetrainPair {
             left: output_linear - output_angular,
